c8847a415e1b66d764e6801ee3e5e5fb482f38e8
adarshpanwar5 <adarshpanwar9622@gmail.com>
2025-09-13 21:43:12 +0530

    Added AdminDashBoard comonent

---
+++ src/components/others/AllTask.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div className='bg-[#1c1c1c] p-5 rounded mt-15 h-60 overflow-auto'>
+        <div className='bg-pink-400 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Adarsh</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+
+         <div className='bg-indigo-400 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Adarsh</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+
+         <div className='bg-yellow-400 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Adarsh</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+
+         <div className='bg-rose-400 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Adarsh</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+         
+         <div className='bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Adarsh</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+    </div>
+  )
+}
+
+export default AllTask
